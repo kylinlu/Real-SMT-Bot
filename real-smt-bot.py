41480e78a16015dbd09fc148b26f5c05793bece3
Kevin Lu <klu12@umd.edu>
2018-12-19 23:57:15 -0500

    changed response

--- real-smt-bot.py
+++ real-smt-bot.py
@@ -18,7 +18,7 @@ def bot_login():
 def run_bot(r, comments_replied_to):
 	for comment in r.subreddit("all").comments(limit = None):
 		if "persona" in comment.body.lower() and comment.id not in comments_replied_to and comment.author != r.user.me() and comment.author != "GoodBot_BadBot":
-			comment.reply("Persona? Play a real [Sheen Megoomi Tensay](https://youtu.be/TepXZN3Pw8o) \n***\n^I'm ^a ^bot *^bleep, ^bloop*")
+			comment.reply("Play a real [Sheen Megoomi Tensay](https://youtu.be/TepXZN3Pw8o) \n***\n^I'm ^a ^bot *^bleep, ^bloop*")
 			comments_replied_to.append(comment.id)
 
 			with open("comments_replied_to.txt", "a") as f:
