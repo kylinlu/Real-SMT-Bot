5d3b5de91278e8e05e9b881e84feef06be3d4c31
䂊汐 <28.juxi.lu@gmail.com>
2019-02-22 21:06:34 -0500

    responds only to whole words of "persona" e.g. nothing like "impersonating"

--- real-smt-bot.py
+++ real-smt-bot.py
@@ -17,8 +17,8 @@ def bot_login():
 
 def run_bot(r, comments_replied_to):
 	for comment in r.subreddit("test").comments(limit = None):
-		if "persona" in comment.body.lower() and comment.id not in comments_replied_to and comment.author != r.user.me() and comment.subreddit != "megaten" and "bot" not in comment.author.name.lower():
-			comment.reply("Weeb, play a real [Sheen Megoomi Tensay](https://youtu.be/zIjVvnO5lgM) \n\nLook at how [cool](https://youtu.be/ut9ekAp2drs) and [sexy](https://youtu.be/TepXZN3Pw8o) mainline SMT is! \n\n Here's Strange Journey's OP just [because](https://youtu.be/09Ty1p9tQEQ)\n\n(And in case you thought *any* bit of Persona was hard, [LOL](https://youtu.be/J3ZMnOx5tzU)) \n\n Sincerely, r/Megaten \n***\n^I'm ^a ^bot *^bleep, ^bloop* ^| ^downvote ^to ^remove")
+		if "persona" in comment.body.lower().split() and comment.id not in comments_replied_to and comment.author != r.user.me() and comment.subreddit != "megaten" and "bot" not in comment.author.name.lower():
+			comment.reply("Weeb, play a real [Sheen Megoomi Tensay](https://youtu.be/zIjVvnO5lgM) \n\nHumanity's filth brought out this anti-Persona [Schwarzwelt](https://youtu.be/09Ty1p9tQEQ) \n\nJust look at how [cool](https://youtu.be/ut9ekAp2drs) and [sexy](https://youtu.be/TepXZN3Pw8o) mainline Shin Megami Tensei is! \n\n(And in case you thought *any* bit of Persona was hard, [LOL](https://youtu.be/J3ZMnOx5tzU)) \n\nSincerely, r/Megaten \n\n***\n^I'm ^a ^bot *^bleep, ^bloop* ^| ^downvote ^to ^remove")
 			comments_replied_to.append(comment.id)
 
 			with open("comments_replied_to.txt", "a") as f:
@@ -41,6 +41,6 @@ comments_replied_to = get_saved_comments()
 while True:
 	try:
 		run_bot(r, comments_replied_to)
+
 	except Exception as e:
-		print(e)
 		continue
